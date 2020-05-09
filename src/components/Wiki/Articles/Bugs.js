0c03cf67d7fbc4882163b8e7bfdad4e50345752d
Derek Pyle <derekpyle@gmail.com>
2020-05-09 14:42:57 -0400

    more bug fixes

--- src/components/Wiki/Articles/Bugs.js
+++ src/components/Wiki/Articles/Bugs.js
@@ -11,10 +11,11 @@ const bugs = () => {
             <div className="wiki__content-box__article__info">
             <ul className="wiki__content-box__article__info__list wiki__content-box__article__info__list--bugs">
                 <li className="wiki__content-box__article__info__list__item wiki__content-box__article__info__list__item--bugs--1">Shadow Slayer quest line currently broken</li>
-                <li className="wiki__content-box__article__info__list__item wiki__content-box__article__info__list__item--bugs--2">Quest Timers currently off, will be fixed next reset</li>
+                <li className="wiki__content-box__article__info__list__item wiki__content-box__article__info__list__item--bugs--2">Quest Timers currently off.  **Fixed with 5/9/20 reset**</li>
                 <li className="wiki__content-box__article__info__list__item wiki__content-box__article__info__list__item--bugs--3">Tier 8 Axe max damage should be 74, but does not drop.  *GDLE bug*</li>
                 <li className="wiki__content-box__article__info__list__item wiki__content-box__article__info__list__item--bugs--3">Void wands dropping with war weild reqs.  *GLDE bug*</li>
                 <li className="wiki__content-box__article__info__list__item wiki__content-box__article__info__list__item--bugs--3">When giving an Aug Gem to Donatello Linante, if you have >8b unspent xp he will take 8b rather than 2b</li>
+                <li className="wiki__content-box__article__info__list__item wiki__content-box__article__info__list__item--bugs--3">Aun Ralirea currently only respawns after server resets</li>
                 <li className="wiki__content-box__article__info__list__item wiki__content-box__article__info__list__item--bugs--3">If you do not have 1b unspent when giving Asheron a blank aug gem, he will keep it</li>
                 
             </ul>
