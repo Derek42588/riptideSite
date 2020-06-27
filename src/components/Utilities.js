54f9ce481b82ecbb2fd312b9b6ab2d4f28b22d8e
Derek Pyle <derekpyle@gmail.com>
2020-06-27 13:50:53 -0400

    adding ELO plugin

--- src/components/Utilities.js
+++ src/components/Utilities.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
+import eloPlugin from '../assets/Riptide_Elo.dll'
 
 
 const Utilities = () => {
@@ -15,6 +16,11 @@ const Utilities = () => {
             <li className="utilities__instructions-item"><a href="https://utilitybelt.gitlab.io/">Utility Belt</a></li>
             <li className="utilities__instructions-item"><a href="http://www.immortalbob.com/phpBB3/download/file.php?id=60&sid=9c73ad1eeb734ac9df849af08e7654aa">Loot Snob Profile</a></li>
             <li className="utilities__instructions-item">Things you will not find here (ask your guild): Relogger, PK targetting plugin, metas</li>
+            <li className="utilities__instructions-item"><Link to={eloPlugin} target="_blank" download>Elo Plugin</Link></li>
+            <li className="utilities__instructions-item">ELO plugin instructions:</li>
+            <li className="utilities__instructions-item">To refresh the ELO type "/elo" (There is a 5 minute lockout on this command to prevent people from spamming the server)</li>
+            <li className="utilities__instructions-item">Clicking on a name in the Top Pks list will populate the "Search" tab with their stats.</li>
+            <li className="utilities__instructions-item">Top Ranked PKs shows the top 100.</li>
             </ul>
 
     </div>
